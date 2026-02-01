4c6073e5d3eaa19ed2b689b9aae048b95ad98222
LAPTOP_DEV\funes <funes_garcia@hotmail.com>
2026-02-01 14:00:26 -0600

    Boton terciario creado

--- ProyectoPrograMovilPer/App.tsx
+++ ProyectoPrograMovilPer/App.tsx
@@ -3,30 +3,44 @@ import { StyleSheet, Text, View } from 'react-native';
 import CustomButton from './src/components/CustomButton';
 
 export default function App() {
-
   const handleOnLogin = () => {
-    console.log(" prueba desde app");
-    alert("Tu Iphone ha sido actualizado");
-
+    console.log("prueba login desde app");
+    alert("Alerta login desde app");
   }
 
   const handleOnLogout = () => {
-    console.log("alerta logout desde app")
-    alert("Alerta desde app");
+    alert("Alerta logout desde app");
+  }
+
+  // 1. Definimos la función para recuperar contraseña
+  const handleForgotPassword = () => {
+    alert("Redirigiendo a recuperar contraseña...");
   }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App</Text>
+      <Text>INICIO DE SESION</Text>
       <StatusBar style="auto" />
-      <CustomButton 
-      title = {'login'}
-      onClick={handleOnLogin}
-      />
-      <CustomButton 
-      title = {'Salir'} 
-      onClick={handleOnLogout}
-      />
+      <View style={styles.buttonsWrapper}>
+        <View style={styles.card}>
+          <CustomButton 
+            title={'Login'} 
+            onClick={handleOnLogin} 
+          />
+          <CustomButton 
+            title={'Salir'}
+            onClick={handleOnLogout}
+            variant={'secondary'} 
+          />
+          
+         
+          <CustomButton 
+            title={'¿Has olvidado la contraseña?'}
+            onClick={handleForgotPassword}
+            variant={'forgotPassword'} 
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -34,8 +48,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ffff'
+  },
+  card:{
+    width: "80%",
+    height: "80%",
+    borderRadius: 15,
+    backgroundColor:'#e9e9e9',
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingVertical: 10,
+  },
+  buttonsWrapper:{
+    marginTop: 15,
+    width: "80%",
+    height: "40%",
+    alignItems: "center",
   },
 });
--- ProyectoPrograMovilPer/src/components/CustomButton.tsx
+++ ProyectoPrograMovilPer/src/components/CustomButton.tsx
@@ -1,35 +1,49 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-interface CustomButtonProps { //aqui van variables
+interface CustomButtonProps {
     title: string;
     onClick: () => void;
+    variant?: "primary" | "secondary" | "forgotPassword"; 
 }
 
 export default function CustomButton ({
-    title, onClick
+    title,
+    onClick,
+    variant = "primary"
 }: CustomButtonProps){
-
+    const styles = getStyles(variant);
+ 
     return(
-        <TouchableOpacity
-        onPress={onClick}
-        style={styles.container}> //hay que usar llaves dentro de las etiquetas
-            <Text 
-            style={styles.text}>
-            {title}
-            </Text>
+       <TouchableOpacity
+            onPress={onClick}
+            style={styles.container}>
+                <Text
+                style={styles.text}>
+                    {title}
+                </Text>
         </TouchableOpacity>
     );
 }
 
-const styles = StyleSheet.create({
+const getStyles = (variant: 'primary' | 'secondary' | 'forgotPassword' ) => 
+    StyleSheet.create({
     container: {
-        backgroundColor: "#2e4566",
+        paddingVertical: 15,
+        alignItems: 'center',
         width: "80%",
-        borderWidth: 2,
+        backgroundColor: 
+            variant === "primary" ? "#1be7c8" : 
+            variant === "secondary" ? "red" : "transparent",
         borderRadius: 8,
-
+        borderWidth: variant === "forgotPassword" ? 0 : 1,
+        borderColor: "#ffffff",
     },
     text: {
-        color: "white",
+        color: 
+            variant === "primary" ? "white" : 
+            variant === "secondary" ? "white" : "#1be7c8",
+        fontSize: variant === "forgotPassword" ? 14 : 18,
+        fontWeight: "500",
+        textDecorationLine: variant === "forgotPassword" ? "underline" : "none"
     }
 });
